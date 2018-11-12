4e1134f55f5afac5a5b0671b18a56ace59971d76
Jim Tsai <jimtsai@Jims-MacBook-Pro-2.local>
2018-11-12 18:07:59 -0500

    Updates

---
+++ precache-manifest.1709cf732d14c45498abd38372d1699c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8977b93de745db65e653c1284cfab431",
+    "url": "/sunshine/static/media/weather-sunny.8977b93d.svg"
+  },
+  {
+    "revision": "166c233267f23dfd10eb",
+    "url": "/sunshine/static/css/main.d45490d1.chunk.css"
+  },
+  {
+    "revision": "0ceb25d4a19589731e9e",
+    "url": "/sunshine/static/js/1.0ceb25d4.chunk.js"
+  },
+  {
+    "revision": "5ef5f466fcebb3b14eff",
+    "url": "/sunshine/static/js/runtime~main.5ef5f466.js"
+  },
+  {
+    "revision": "eaabc701e35a410bd5b1c4d3d66bd19a",
+    "url": "/sunshine/static/media/weather-cloudy.eaabc701.svg"
+  },
+  {
+    "revision": "ce6541a5ce0f05e2d2b025f4efd4a9d2",
+    "url": "/sunshine/static/media/weather-rainy-2.ce6541a5.svg"
+  },
+  {
+    "revision": "166c233267f23dfd10eb",
+    "url": "/sunshine/static/js/main.166c2332.chunk.js"
+  },
+  {
+    "revision": "226abcba6cb6887972985405f125fcac",
+    "url": "/sunshine/static/media/menu.226abcba.svg"
+  },
+  {
+    "revision": "2aa6b832742a549e569e143d3772e240",
+    "url": "/sunshine/static/media/weather-stormy.2aa6b832.svg"
+  },
+  {
+    "revision": "f23082d039608619f691760396e3474e",
+    "url": "/sunshine/static/media/weather-rainy.f23082d0.svg"
+  },
+  {
+    "revision": "87c93ad62cad8d5e50f627be59be5345",
+    "url": "/sunshine/static/media/weather-moon.87c93ad6.svg"
+  },
+  {
+    "revision": "7c5fe09e714fa9b8980875d15e836516",
+    "url": "/sunshine/static/media/weather-cloudy-night.7c5fe09e.svg"
+  },
+  {
+    "revision": "26ead8d80b145dd6d0b9c8e1468d91fc",
+    "url": "/sunshine/index.html"
+  }
+];
